f8af4a9436fc168346bfbf9c6726cf88d447ad56
Anmarie Smith <anmariesmith007@gmail.com>
2018-09-30 22:03:11 -0400

    added functionality to add magnify button, added formatting for verify, update and delte

--- unsolved/app.js
+++ unsolved/app.js
@@ -7,10 +7,14 @@ const render = function () {
 }
 render();
 //When the view button is clicked,it will render the full list: 
-const showView= function(){
+const showView = function () {
     $('#addContact').addClass('hide');
     $('.content').removeClass('hide');
     $('#verifyBox').addClass('hide');
+    $('#verifyWord').addClass('hide');
+    $('#verifyWord2').addClass('hide');
+    $('#updateContact').addClass('hide');
+    $('#deleteContact').addClass('hide');
 }
 $('#view').on('click', showView);
 
@@ -21,42 +25,110 @@ const showAdd = function () {
     $('#addContact').removeClass('hide');
     $('.content').removeClass('hide');
     $('#verifyBox').addClass('hide');
+    $('#verifyWord').addClass('hide');
+    $('#verifyWord2').addClass('hide');
+    $('#updateContact').addClass('hide');
+    $('#deleteContact').addClass('hide');
 }
 $('#add').on('click', showAdd);
 
 // This will take the input from the "add bar" and add to the list , and render the new list
-// const getInputVal= function(){
-//     const nameVal= $('#addName').val();
-//     const officeVal= $('#addOfficeNum').val();
-//     const phoneVal= $('#addPhoneNum').val();
-//     employeeList.push(nameVal));
-//     employeeList.push(officeVal));
-//     employeeList.push(phoneVal));
+const getInputVal = function () {
+    const nameVal = $('#addName').val();
+    const officeVal = $('#addOfficeNum').val();
+    const phoneVal = $('#addPhoneNum').val();
+    employeeList.push({
+        name: nameVal,
+        officeNum: officeVal,
+        phoneNum: phoneVal
+    });
 
-//     $('#addName').val('');
-//     $('#addOfficNum').val('');
-//     $('#addPhoneNum').val('');
+    $('#addName').val('');
+    $('#addOfficeNum').val('');
+    $('#addPhoneNum').val('');
 
-//     render();
-// }
-// $('#magnifyButton').on('click',getInputVal);
+    render();
+}
+$('#magnifyButton').on('click', getInputVal);
 
 //Adding Verfiy Box- making sure list and add bar do not show when verify is clicked
 const showVerify = function () {
     $('#verifyBox').removeClass('hide');
     $('#addContact').addClass('hide');
     $('.content').addClass('hide');
+    $('#verifyWord').addClass('hide');
+    $('#verifyWord2').addClass('hide');
+    $('#updateContact').addClass('hide');
+    $('#deleteContact').addClass('hide');
 }
 $('#verify').on('click', showVerify);
 // Adding function to verify button if input matches then display yes or no
 
-const verifyInclude = function(){
+const verifyInclude = function () {
     const verifyName = $('#verifyInput').val();
-    if(employeeList.includes(verifyName)){
-        $('verifyWord').removeClass('hide');
-}
-$('#verifyInput').val('');
-}
+    for (let i = 0; i < employeeList.length; i++) {
+        if (employeeList[i].name === verifyName) {
+            $('#verifyWord').removeClass('hide');
+            $('verifyWord2').addClass('hide');
+        } else
+            $('#verifyWord2').removeClass('hide');
+        $('verifyWord').addClass('hide');
+    };
+    $('#verifyInput').val('');
+};
 
 $('#magnifyButtonVerify').on('click', verifyInclude);
 
+// An Update option that allows the user to input name, office number, and phone number and updates the office number and phone number of the employee that matches the input name, and then renders the updated employee list.
+const showUpdate = function () {
+    $('#addContact').addClass('hide');
+    $('.content').removeClass('hide');
+    $('#verifyBox').addClass('hide');
+    $('#verifyWord').addClass('hide');
+    $('#verifyWord2').addClass('hide');
+    $('#deleteContact').addClass('hide');
+    $('#updateContact').removeClass('hide');
+}
+$('#update').on('click', showUpdate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This will show the "Delete bar when clicked on Delete"
+const showDel = function () {
+    $('#addContact').addClass('hide');
+    $('.content').removeClass('hide');
+    $('#verifyBox').addClass('hide');
+    $('#verifyWord').addClass('hide');
+    $('#verifyWord2').addClass('hide');
+    $('#updateContact').addClass('hide');
+    $('#deleteContact').removeClass('hide');
+}
+$('#delete').on('click', showDel);
+
+
+// Delete Option that deletes the employee with the matching name and then renders the updated employee list
+const deleteThis = function () {
+    const nameDel = $('#delName').val();
+    for (let i = 0; i < employeeList.length; i++) {
+        if (employeeList[i].name == nameDel) {
+            delete employeeList.name.nameDel;
+        $('#delName').val('');
+    } else
+    $('#delName').val('');
+}
+render();
+};
+$('#magnifyButtonDelete').on('click', deleteThis);
